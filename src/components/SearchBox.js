8a54d8b18f3df2ae111ceb972a451592862a1364
luiseer <85654418+luiseer@users.noreply.github.com>
2024-08-31 20:38:16 -0600

    [UPDATE] handleSearch

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,16 +4,16 @@ import React, {useState}  from 'react'
 const SearchBox = ({ setLocation }) => {
     const [id, setId] = useState("")
 
-    const search = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault()
         axios
         .get(`https://rickandmortyapi.com/api/location/${id}`)
         .then(res => setLocation(res.data))
     }
     return (
-        <form className='search-box'>
+        <form className='search-box' onSubmit={handleSubmit}>
             <input placeholder='ingresa el numero de planeta' type="text" onChange={(e) => setId(e.target.value)} value={id} />
-            <button className='boton' onClick={search}>Search</button>
+            <button className='boton' >Search</button>
         </form>
     )
 }
