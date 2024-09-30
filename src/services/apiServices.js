2844b545bbc30b184fcb3c98d056e53252694820
luiseer <85654418+luiseer@users.noreply.github.com>
2024-09-29 22:59:36 -0600

    [UPDATE] use services

---
+++ src/services/apiServices.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+const urlLocations = 'https://rickandmortyapi.com/api/location'
+const urlCharacters = 'https://rickandmortyapi.com/api/character'
+
+const getAllLocatiosn = () => {
+    return axios
+        .get(urlLocations)
+        .then(response => {
+            const {data} = response
+            
+        })
+}
